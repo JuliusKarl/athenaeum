4a1cda702f3917d26ccdb3fbde165397821df377
Julius Karl <julius.karl.macrohon@gmail.com>
2020-09-19 20:05:47 +1200

    CICD Pipeline

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace BookStore.Controllers
     {
         public ViewResult Index() 
         {
-            ViewData["Title"] = "Home";
+            ViewData["Title"] = "Athenaeum";
             return View();
         }
         public ViewResult About()
